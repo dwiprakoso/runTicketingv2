0c832171bc98aa9ea62cc3405e96e4ee319aed90
dwiprakoso <dwiprakoso015@gmail.com>
2025-03-02 22:38:20 +0700

    fix seeder

--- database/seeders/TicketCategorySeeder.php
+++ database/seeders/TicketCategorySeeder.php
@@ -17,7 +17,7 @@ class TicketCategorySeeder extends Seeder
             ['name' => 'Fun Run', 'description' => 'Terdapat pilihan jarak 3K dan 7K', 'price' => 250000, 'quota' => 50],
             ['name' => 'Family Run', 'description' => 'Berlari bersama keluarga (1 Dewasa & 1 Anak - Anak)', 'price' => 450000, 'quota' => 200],
             ['name' => 'Kids 3K', 'description' => 'Daftarkan anak anda dengan maksimal usia 12 Tahun untuk berlari 3K', 'price' => 200000, 'quota' => 100],
-            ['name' => 'Early Bird - Fun Run 7K', 'description' => 'Early Bird', 'price' => 225000, 'quota' => 100],
+            ['name' => 'Early Bird - Fun Run 7K', 'description' => 'Tiket Early Bird untuk kategori Fun Run 7K dengan Kuota Terbatas.', 'price' => 225000, 'quota' => 100],
         ]);
     }
 }
